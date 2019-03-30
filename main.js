f4968547c3262aec594823e4c0f9c4741dbaa0e3
mhmalekian <malekian610@gmail.com>
2019-03-30 15:26:37 +0430

    implement isChainValid function - mhmalekian-980110

--- main.js
+++ main.js
@@ -26,7 +26,7 @@ class BlockChain
 
     createGenesisBlock()
     {
-        return new Block(0,"01/01/2019","Gensis block","0");
+        return new Block(0,"01/01/2019","Genesis block","0");
     }
 
     getLastBlock()
@@ -41,6 +41,27 @@ class BlockChain
         newBlock.Hash=newBlock.calculateHash();
         this.chain.push(newBlock);
     }
+
+    isChainValid()
+    {
+        for(let i=1;i<this.chain.length ; i++)
+        {
+            const currentBlock=this.chain[i];
+            const previousBlock=this.chain[i-1];
+
+            if(currentBlock.Hash!==currentBlock.calculateHash())
+            {
+                return false;
+            }
+
+            if(currentBlock.previousHash!==previousBlock.Hash)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
 }
 
 
@@ -49,4 +70,6 @@ mhmCoin.addBlock(new Block(1,"02/01/2019",{amont: 4}));
 mhmCoin.addBlock(new Block(2,"02/03/2019",{amont: 6}));
 mhmCoin.addBlock(new Block(1,"05/04/2019",{amont: 8}));
 
-console.log(JSON.stringify(mhmCoin,null,4));
+//console.log(JSON.stringify(mhmCoin,null,4));
+
+console.log("mhmCoin BlockChain is valid? "+mhmCoin.isChainValid());
