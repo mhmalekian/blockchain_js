1f8df64fe2bf63fd3530e2cf5f48d074bec27b73
mhmalekian <malekian610@gmail.com>
2019-03-30 15:11:59 +0430

    implement Block and BlockChain and a simple sample mhmCoin - mhmalekian-980110

--- main.js
+++ main.js
@@ -8,10 +8,10 @@ class Block
         this.timestamp=timestamp;
         this.data=data;
         this.previousHash=previousHash;
-        this.Hash=this.calculatHas();
+        this.Hash=this.calculateHash();
     }
 
-    calculatHas()
+    calculateHash()
     {
         return SHA256(this.index+this.previousHash+this.timestamp+JSON.stringify(this.data)).toString();
     }
@@ -28,4 +28,25 @@ class BlockChain
     {
         return new Block(0,"01/01/2019","Gensis block","0");
     }
+
+    getLastBlock()
+    {
+        return this.chain[this.chain.length -1];
+
+    }
+
+    addBlock(newBlock)
+    {
+        newBlock.previousHash=this.getLastBlock().Hash;
+        newBlock.Hash=newBlock.calculateHash();
+        this.chain.push(newBlock);
+    }
 }
+
+
+let mhmCoin = new BlockChain();
+mhmCoin.addBlock(new Block(1,"02/01/2019",{amont: 4}));
+mhmCoin.addBlock(new Block(2,"02/03/2019",{amont: 6}));
+mhmCoin.addBlock(new Block(1,"05/04/2019",{amont: 8}));
+
+console.log(JSON.stringify(mhmCoin,null,4));
