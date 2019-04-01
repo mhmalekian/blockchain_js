bd642efcdbe399d00b7eaae832e6655fae03e497
mhmalekian <malekian610@gmail.com>
2019-04-02 01:16:08 +0430

    increase difficulty - mhmalekian-980112

--- main.js
+++ main.js
@@ -34,7 +34,7 @@ class BlockChain
     constructor()
     {
         this.chain=[this.createGenesisBlock()];
-        this.difficulty=2;
+        this.difficulty=5;
     }
 
     createGenesisBlock()
